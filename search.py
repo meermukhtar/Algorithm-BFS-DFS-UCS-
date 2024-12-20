f8cf2a3adb8fa57bfc8b6e62d9c7062716d4fd59
Ameer Mukhtar <100961807+meermukhtar@users.noreply.github.com>
2024-12-21 02:12:00 +0500

    Update search.py

--- search.py
+++ search.py
@@ -10,7 +10,7 @@
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
-#search agent
+
 
 """
 In search.py, you will implement generic search algorithms which are called by
